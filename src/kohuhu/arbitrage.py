8324241ac5ee1429b054fd01dea836876621ca9e
Kevin Doran <kevinxdoran@gmail.com>
2017-12-21 13:53:29 +1300

    Fleshing out a simple arbitrage using the new framework.

--- src/kohuhu/arbitrage.py
+++ src/kohuhu/arbitrage.py
@@ -1,4 +1,7 @@
 import kohuhu.trader as trader
+from kohuhu.trader import OrderAction
+import kohuhu.exchanges as exchanges
+from decimal import Decimal
 
 class OneWayPairArbitrage(trader.Algorithm):
 
@@ -7,7 +10,10 @@ class OneWayPairArbitrage(trader.Algorithm):
         super().__init__()
         self.exchange_buy_on = None
         self.exchange_sell_on = None
-        self.live_limit_orders = []
+        self.live_limit_orders = True
+        self.max_bid_amount_in_btc = Decimal(0.5)
+        self.bid_amount_in_btc = Decimal(0.5)
+        self.profit_target = 0.05 # percent.
 
     def initialize(self, exchanges_to_use):
         if len(exchanges_to_use) != 2:
@@ -16,11 +22,56 @@ class OneWayPairArbitrage(trader.Algorithm):
         self.exchange_buy_on = exchanges_to_use[0]
         self.exchange_sell_on = exchanges_to_use[0]
 
+    # Note: should the slice contain balance info? Or maybe we could include
+    # the fetchers as part of the input. We need some sort of on demand data,
+    # I think.
     def on_data(self, slice):
-        if not len(self.live_limit_orders):
+        if not self.live_limit_orders:
+            # Caculate the limit order price.
+            order_book = slice.for_exchange(self.exchange_sell_on).order_book
+            available_bid_amount = Decimal(0)
+            bid_index = Decimal(0)
+            effective_market_price = Decimal(0)
+            filled = Decimal(0)
+            remaining = self.bid_amount_in_btc
+            while available_bid_amount < self.bid_amount_in_btc:
+                next_highest_bid = order_book['bids'][bid_index]
+                price = Decimal(next_highest_bid[0])
+                amount = max(Decimal(next_highest_bid[1]), remaining)
+                fraction_of_trade = amount / self.bid_amount_in_btc
+                effective_market_price += fraction_of_trade * price
 
-        my_orders = slice.for_exchange(self.exchange_buy_on)
-        for o in my_orders:
+            bid_price = self.calculate_bid_limit_price(self.exchange_buy_on,
+                                                       self.exchange_sell_on,
+                                                       effective_market_price,
+                                                       self.profit_target)
+            bid_action = OrderAction(OrderAction.Side.BID,
+                                     OrderAction.Type.Limit,
+                                     amount=self.bid_amount_in_btc,
+                                     price=bid_price)
+            self.live_limit_orders = True
+            return bid_action
+        else:
+            # TODO check to see if the order has been met.
 
 
+    @staticmethod
+    def calculate_bid_limit_price(cls, exchange_to_buy_on, exchange_to_sell_on,
+                                  market_price_to_sell, profit_target):
+        buy_maker_fee = exchanges.fees(exchange_to_buy_on)[0]
+        sell_taker_fee = exchanges.fees(exchange_to_sell_on)[1]
 
+        # Calculate bid limit price.
+        # TODO: work through an example on each line.
+        # Create bid limit on e1, sell at market on e2
+        # Note: fee_factor is always less than 1. E.g., fee listed as 0.8
+        # becomes a fee factor: 1/(1+0.08) = 0.926
+        # fee_factor = (1/(1+e2.taker_fee) * 1/(1+e1.maker_fee)))
+        # profit_factor = (e2.highest_bid/bid_limit) * fee_factor
+        # profit_factor / fee_factor = e2.highest_bid/bid_limit
+        # fee_factor / profit_factor = bid_limit / e2.highest_bid
+        # bid_limit = fee_factor * e2.highest_bid / profit_factor
+        fee_factor = exchanges.fee_as_factor(buy_maker_fee) * \
+                     exchanges.fee_as_factor(sell_taker_fee)
+        bid_limit = fee_factor * market_price_to_sell / profit_target
+        return bid_limit
--- src/kohuhu/trader.py
+++ src/kohuhu/trader.py
@@ -1,3 +1,4 @@
+from enum import Enum, auto
 
 class ExchangeSlice:
     """Data from a single at a point in time.
@@ -5,15 +6,70 @@ class ExchangeSlice:
     Note: I'm not sure what data is needed and how granular it should be given.
     I just guessed at some simple methods to begin with.
     """
-    def __init__(self):
-        pass
+    def __init__(self, fetcher):
+        # Lets assume that the order book, personal orders and balance use the
+        # ccxt structure.
+        self._order_book = None
+        self._personal_orders = None
+        self._balance = None
+        self.fetcher = fetcher
+        self.always_fetch_order_book = True
+        self.always_fetch_personal_orders = True
+        self.always_fetch_balance = False
 
+    def populate(self):
+        self._order_book = None
+        self._personal_orders = None
+        self._balance = None
+        if self.always_fetch_order_book:
+            self._order_book = self.fetcher.get_order_book()
+        if self.always_fetch_personal_orders:
+            self._order_book = self.fetcher.get_personal_orders()
+        if self.always_fetch_balance:
+            self._order_book = self.fetcher.get_balance()
+
+    @property
     def order_book(self):
-        raise NotImplementedError("TODO")
+        """
+        The order book for the exchange.
 
+        Example order book:
+        {
+            'bids': [
+                [ price, amount ],
+                [ price, amount ],
+                ...
+            ],
+            'asks': [
+                [ price, amount ],
+                [ price, amount ],
+                ...
+            ],
+            'timestamp': 1499280391811, // Unix Timestamp in milliseconds (seconds * 1000)
+            'datetime': '2017-07-05T18:47:14.692Z', // ISO8601 datetime string with milliseconds
+        }
+        """
+        if not self._order_book:
+            if self.always_fetch_order_book:
+                raise Exception("The order book should have been pre-fetched.")
+            self._order_book = self.fetcher.get_order_book()
+        return self._order_book
+
+    @property
     def personal_orders(self):
-        raise NotImplementedError("TODO")
+        if not self.personal_orders:
+            if self.always_fetch_personal_orders:
+                raise Exception("Personal orders should have been pre-fetched.")
+            self._personal_orders = self.fetcher.get_personal_orders()
+        return self._personal_orders
 
+    @property
+    def balance(self):
+        if not self._balance:
+            if self.always_fetch_balance:
+                raise Exception("Balance should have been pre-fetched.")
+            self._balance = self.fetcher.get_balance()
+        return self._balance
 
 class Algorithm:
     """Subclass Algorithm and pass it to Trader to make trades.
@@ -41,24 +97,58 @@ class Action:
     def __init__(self):
         pass
 
+    @property
     def name(self):
+        """The name of the action."""
         raise NotImplementedError("Subclasses should implement this method.")
 
     def __repr__(self):
         raise NotImplementedError("Subclasses should implement this method.")
 
 
-class LimitOrder(Action):
+class OrderAction(Action):
+    """Represents the action of creating an order."""
 
-    def __init__(self):
+    class Side(Enum):
+        ASK = auto()
+        BID = auto()
+
+    class Type(Enum):
+        Market = auto()
+        Limit = auto()
+
+    def __init__(self, side, type, amount, price):
         super().__init__()
+        self.amount = amount
+        self.price = price
+        self.side = side
+        self.type = type
 
+    @Action.name.getter
     def name(self):
-        pass
+        side_name = self.side.name.lower
+        side_name = side_name[:1].upper() + side_name[1:]
+        type_name = self.type.name.lower
+        type_name = type_name[:1].upper() + type_name[1:]
+        return "{} {} order".format(type_name, side_name)
 
     def __repr__(self):
-        pass
+        return "{} for {} BTC at {} $/BTC".format(self.name, self.amount,
+                                                  self.type)
 
+class CancelOrder(Action):
+    """Represents the action of cancelling an order."""
+
+    def __init__(self, order_id):
+        super().__init__()
+        self.order_id = order_id
+
+    @Action.name.getter
+    def name(self):
+        return "Cancel order"
+
+    def __repr__(self):
+        return "{} for order ID: {}".format(self.name, self.order_id)
 
 class Executor:
     """Executes actions for one or multiple exchange."""
@@ -96,20 +186,26 @@ class Fetcher:
         """
         self.supported_exchanges = supported_exchanges
 
-    def fetch(self, exchange):
-        """Retrieves and parsers the data for the given exchange into an
-        ExchangeSlice.
+    def get_balance(self, exchange):
+        self._check_support(exchange)
+        raise Exception("Not implemented yet.")
+
+    def get_personal_orders(self, exchange):
+        self._check_support(exchange)
+        raise Exception("Not implemented yet.")
+
+    def get_order_book(self, exchange):
+        """Retrieves an order book.
 
         Args:
             exchange (ccxt.Exchange): the exchange to fetch the data for.
-
-        Returns:
-            (ExchangeSlice): the ExchangeSlice for the given exchange.
         """
-        if exchange not in self.supported_exchanges:
+        self._check_support(exchange)
+
+    def _check_support(self, exchange_id):
+        if exchange_id not in self.supported_exchanges:
             raise Exception("This fetcher doesn't support the exchange {}."
-                            .format(exchange))
-        raise NotImplementedError("Subclasses should implement this method.")
+                            .format(exchange_id))
 
 
 class Slice:
@@ -157,12 +253,16 @@ class Trader:
         self._algorithm.initialize(self.exchanges)
 
     def fetch_next_slice(self):
-        """"""
+        """Fetches data from the exchanges.
+
+        When testing, this method doesn't need to be called, and the data
+        can be set directly.
+        """
         next_slice = Slice()
         for id in self.exchanges:
-            if id in self._fetchers:
-                exchange_slice = self._fetchers[id]
-                next_slice.exchange_slices[id] = exchange_slice
+            fetcher = self._fetchers[id]
+            exchange_slice = ExchangeSlice(fetcher)
+            next_slice.set_slice(id, exchange_slice)
         self._slices.append(next_slice)
 
     def add_slice(self, slice):
